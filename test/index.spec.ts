91afa919a439bc895e0eb1b24e7547c7cd492656
Tyler Liu <tyler4long@gmail.com>
2023-11-17 17:39:50 -0800

    More screenshots

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,7 +5,19 @@ import preload from './preload';
 const $ = preload();
 
 test('0', async () => {
-  await $.page.waitForTimeout(10000);
   expect(await $.page.title()).toEqual('Assets Hosting');
-  await $.page.waitForTimeout(10000);
+});
+
+test('1', async () => {
+  expect(await $.page.title()).toEqual('Assets Hosting');
+  await $.page.click('text=Login via GitHub');
+  await $.page.waitForTimeout(3000);
+});
+
+test('2', async () => {
+  expect(await $.page.title()).toEqual('Assets Hosting');
+  await $.page.click('text=Login via GitHub');
+  await $.page.waitForTimeout(3000);
+  await $.page.click('text=tyler4longdemo/demo');
+  await $.page.waitForTimeout(3000);
 });
